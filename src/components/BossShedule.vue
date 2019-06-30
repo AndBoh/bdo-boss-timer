5beb3f47890581ced088bbee667a3bf1767177fc
Andrey Bokhan <andrbokhan@gmail.com>
2019-06-30 22:35:43 +0300

    State moved to Vuex

--- src/components/BossShedule.vue
+++ src/components/BossShedule.vue
@@ -1,24 +1,18 @@
 <template>
     <div class="boss-shedule">
         {{time}}
-        {{bossNextRespawn}}
+        {{bossRespawnList}}
     </div>
 </template>
 
 <script>
 
-const MS_Per_Hour = 1000*60*60;
-const MS_Per_Day = MS_Per_Hour*24;
+
 
 import {mapGetters, mapState, mapActions, mapMutations} from 'vuex';
 import { setInterval } from 'timers';
 
-function getWeekBegin(weekOffset = 0) {
-    let today = new Date();
-    let weekBegin = new Date(today.getFullYear(),today.getMonth(),today.getDate()-((today.getDay() || 7)));
-    weekBegin.setDate(weekBegin.getDate()+7*weekOffset);
-    return weekBegin;
-}
+
 
 export default {
    name: 'BossShedule',
@@ -28,82 +22,40 @@ export default {
    },
    data () {
        return{
-           timer: undefined,
-           shedule: [],
-           time: Date.now(),
+           timer: null
        }
    },
    computed: {
-       ...mapGetters([]),
-       ...mapState([]),
-       bossNextRespawn() {
-           let bossRespawnList = [];
-           let thisWeekBegin = getWeekBegin();
-           let nextWeekBegin = getWeekBegin(1);
-           this.shedule.forEach(boss => {
-               let bossRespawns = [];
-               boss.bossRespawn.forEach(resp => {
-                   bossRespawns.push(+thisWeekBegin+resp.day*MS_Per_Day+resp.hour*MS_Per_Hour);
-                   bossRespawns.push(+nextWeekBegin+resp.day*MS_Per_Day+resp.hour*MS_Per_Hour);
-               });
-               
-               bossRespawns = bossRespawns.filter((resp) => {
-                   return (resp > +this.time);
-               })
-               
-               bossRespawns.sort();
-               bossRespawnList.push({
-                   bossId: boss.bossId,
-                   bossRespawn: bossRespawns[0]-this.time
-               })
-           });
-           return bossRespawnList;
-       }
+       ...mapGetters([
+           'bossRespawnList'
+       ]),
+       ...mapState([
+           'bossShedule',
+           'time'
+       ]),
    },
    methods: {
-       ...mapActions([]),
-       ...mapMutations([]),
-       updateTime() {
-           this.time = Date.now();
+       ...mapActions([
+           'loadBossShedule'
+       ]),
+       ...mapMutations([
+           'UPDATE_TIME'
+       ]),
+
+       startTimer() {
+        this.timer = setInterval(() => {
+            this.UPDATE_TIME();
+        }, 1000)
        }
+       
    },
    watch: {
 
    },
-   mounted() {
-    //    AJAX подгрузка с сервера
-    this.shedule = [
-        {
-            bossId: 0,
-            bossRespawn: [
-                {
-                    day: 1,
-                    hour: 13
-                },
-                {
-                    day: 3,
-                    hour: 20
-                }
-            ]
-        },
-        {
-            bossId: 1,
-            bossRespawn: [
-                {
-                    day: 2,
-                    hour: 15
-                },
-                {
-                    day: 7,
-                    hour: 20
-                }
-            ]
-        }
-    ];
-    
-    this.timer = setInterval(() => {
-        this.updateTime();
-    }, 1000)
+   mounted() {   
+    this.loadBossShedule();
+    this.UPDATE_TIME();
+    this.startTimer();
    }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,18 +3,87 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const MS_Per_Hour = 1000*60*60;
+const MS_Per_Day = MS_Per_Hour*24;
+
+function getWeekBegin(weekOffset = 0) {
+  let today = new Date();
+  let weekBegin = new Date(today.getFullYear(),today.getMonth(),today.getDate()-((today.getDay() || 7)));
+  weekBegin.setDate(weekBegin.getDate()+7*weekOffset);
+  return weekBegin;
+}
+
 export const store = new Vuex.Store({
   state: {
-
+    bossShedule: [],
+    time: null
   },
   getters: {
+    bossRespawnList(state) {
+      let bossRespawnList = [];
+           let thisWeekBegin = getWeekBegin();
+           let nextWeekBegin = getWeekBegin(1);
+           state.bossShedule.forEach(boss => {
+               let bossRespawns = [];
+               boss.bossRespawn.forEach(resp => {
+                   bossRespawns.push(+thisWeekBegin+resp.day*MS_Per_Day+resp.hour*MS_Per_Hour);
+                   bossRespawns.push(+nextWeekBegin+resp.day*MS_Per_Day+resp.hour*MS_Per_Hour);
+               });
+               
+               bossRespawns = bossRespawns.filter((resp) => {
+                   return (resp > +state.time);
+               });
 
+               bossRespawnList.push({
+                   bossId: boss.bossId,
+                   bossRespawn: Math.min(...bossRespawns)-state.time
+               });
+           });
+           return bossRespawnList;
+    }
   },
   mutations: {
-
+    SET_BOSS_SHEDULE(state, bossShedule) {
+      state.bossShedule = bossShedule
+    },
+    UPDATE_TIME(state) {
+      state.time = Date.now();
+    }
   },
   actions: {
-
+    loadBossShedule(context) {
+      let shedule 
+      // AJAX
+      shedule = [
+        {
+            bossId: 0,
+            bossRespawn: [
+                {
+                    day: 1,
+                    hour: 13
+                },
+                {
+                    day: 3,
+                    hour: 20
+                }
+            ]
+        },
+        {
+            bossId: 1,
+            bossRespawn: [
+                {
+                    day: 2,
+                    hour: 15
+                },
+                {
+                    day: 7,
+                    hour: 20
+                }
+            ]
+        }
+    ];
+      context.commit('SET_BOSS_SHEDULE', shedule)  
+    }
   },
   modules: {
 
